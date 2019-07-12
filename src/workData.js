f078afa6d012a81d80133fcba42037a8e652fe9d
Natalie Despain <despain.natalie@gmail.com>
2019-07-12 15:02:11 -0600

    fix emails

--- src/workData.js
+++ src/workData.js
@@ -31,7 +31,7 @@ export const work = [
     image: <img src={ atlas } alt="Atlas Standard" />,
     blob: <img src={ atlasBlob } alt="Atlas Standard" />,
     color: '#001489',
-    url: 'https://www.atlasstandard.com/',
+    url: 'https://atlas-standard.com/',
     title: 'Atlas Standard',
     detail: 'Store Setup / Theme Customization / Development',
     description: 'Atlas Standard is a high quality clothing brand focusing on protection, comfort, and style. Incorporated a modern and elegant design to showcase the clothing and provide a seamless customer experience on desktop and mobile.'
@@ -40,7 +40,7 @@ export const work = [
     image: <img src={ kuju } alt="Kuju Coffee" />,
     blob: <img src={ kujuBlob } alt="Kuju Coffee" />,
     color: '#6BBFDB',
-    url: 'https://www.kujucoffee.com/',
+    url: 'https://kujucoffee.com/',
     title: 'Kuju Coffee',
     detail: 'Store Setup / Design / Theme Customization / Development / Subscription Models',
     description: 'Delivered Kuju Coffee a site to showcase their lifestyle instant coffee brand with custom homepage and external page layouts focusing on imagery and product usage. Incorporated custom Recharge subscription services and design.'
@@ -49,7 +49,7 @@ export const work = [
     image: <img src={ sparitual } alt="Sparitual" />,
     blob: <img src={ sparitualBlob } alt="Sparitual" />,
     color: '#DBCBB6',
-    url: 'https://www.sparitual.com/',
+    url: 'https://sparitual.com/',
     title: 'Sparitual',
     detail: 'Theme Customization / Custom Templates / Development / Wholesale Setup',
     description: 'Styled with muted colors and rich photography, Skin Care brand, Sparitual, showcases their products with subtle animations and unique layouts. Configured a wholesale/professional partners component without the use of a third party app.'
@@ -58,7 +58,7 @@ export const work = [
     image: <img src={ alta } alt="Alta Gracia" />,
     blob: <img src={ altaBlob } alt="Alta Gracia" />,
     color: '#E6AE24',
-    url: 'https://www.altagraciaapparel.com/',
+    url: 'https://altagraciaapparel.com/',
     title: 'Alta Gracia',
     detail: 'Store Setup / Theme Customization / Development',
     description: 'With a focus on living wages and conditions for workers, Do Good clothing brand, Alta Gracia, incorporated a custom theme to showcase clothing being worn and manufactured. Apps configured include store locator, out of stock notice, custom forms, shipstation, and bulk discount.'
@@ -67,7 +67,7 @@ export const work = [
     image: <img src={ burnout } alt="Burnout" />,
     blob: <img src={ burnoutBlob } alt="Burnout" />,
     color: '#B39F8E',
-    url: 'https://www.byeburnout.com/',
+    url: 'https://byeburnout.com/',
     title: 'Burnout',
     detail: 'Custom Animations / Theme Customization / Development',
     description: 'Simple interactive custom theme for candle lifestyle brand, Burnout, relied on fun animation and easy checkout flows. Burnout provides an engaging and unique shopping experience.'
@@ -76,7 +76,7 @@ export const work = [
     image: <img src={ voke } alt="Voke Tabs" />,
     blob: <img src={ vokeBlob } alt="Voke Tabs" />,
     color: '#78A22F',
-    url: 'https://kujucoffee.com/',
+    url: 'https://voketab.com/',
     title: 'Voke Tabs',
     detail: 'Custom Buy Box Development / Subscription Models / Shogun Development',
     description: 'Voke Tabs required an elegant and easy to use buy box that showcased their product with desktop and mobile optimization. Included custom product zoom, subscription services, and template layouts.'
@@ -85,7 +85,7 @@ export const work = [
     image: <img src={ cutler } alt="Cutler Nutrition" />,
     blob: <img src={ cutlerBlob } alt="Cutler Nutrition" />,
     color: '#54205F',
-    url: 'https://kujucoffee.com/',
+    url: 'https://www.cutlernutrition.com/',
     title: 'Cutler Nutrition',
     detail: 'Store Setup / Design / Custom Theme Development',
     description: 'Built for world-class body-builder, Jay Cutler, Cutler Nutrition featured strong imagery and typefaces to showcase supplement information with custom product templates utilizing custom product fields.'
